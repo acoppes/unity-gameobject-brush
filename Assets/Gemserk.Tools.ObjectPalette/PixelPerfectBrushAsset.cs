f88739cd69d3bdf0bb72fa43af0e519833d115f0
Ariel Coppes <ariel.coppes@gmail.com>
2020-11-04 20:28:12 -0300

    started thinking brush API

---
+++ Assets/Gemserk.Tools.ObjectPalette/PixelPerfectBrushAsset.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Gemserk.Tools.ObjectPalette
+{
+    [CreateAssetMenu(menuName = "Object Palette/Pixel Perfect Brush")]
+    public class PixelPerfectBrushAsset : ScriptableBrushBaseAsset
+    {
+        public int dpi;
+    }
+}
