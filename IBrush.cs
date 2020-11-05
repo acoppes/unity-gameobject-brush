f6fa549f88411e835ebe174d706f986cd822bcfb
Ariel Coppes <ariel.coppes@gmail.com>
2020-11-04 22:41:21 -0300

    changed to use new brush API and scriptable objects

--- IBrush.cs
+++ IBrush.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Gemserk.Tools.ObjectPalette
 {
     public interface IBrush
     {
+        void UpdatePosition(Vector2 position);
         
+        void CreatePreview(List<GameObject> prefabs);
+
+        void DestroyPreview();
+
+        void Paint();
     }
 }
--- ScriptableBrushBaseAsset.cs
+++ ScriptableBrushBaseAsset.cs
@@ -1,9 +1,67 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gemserk.Tools.ObjectPalette
 {
+    [CreateAssetMenu(menuName = "Object Palette/Default Brush")]
     public class ScriptableBrushBaseAsset : ScriptableObject, IBrush
     {
+        [NonSerialized]
+        protected Vector2 position;
         
+        [NonSerialized]
+        protected readonly List<GameObject> previewInstances = new List<GameObject>();
+
+        public void UpdatePosition(Vector2 p)
+        {
+            position = p;
+
+            foreach (var previewInstance in previewInstances)
+            {
+                previewInstance.transform.localPosition = p;
+            }
+        }
+
+        public void CreatePreview(List<GameObject> prefabs)
+        {
+            DestroyPreview();
+            foreach (var prefab in prefabs)
+            {
+                #if UNITY_EDITOR
+                var preview = UnityEditor.PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+                preview.transform.localPosition = position;
+                preview.hideFlags = HideFlags.NotEditable;
+                preview.tag = "EditorOnly";
+                previewInstances.Add(preview);
+                #endif
+            }
+        }
+
+        public void DestroyPreview()
+        {
+            foreach (var previewInstance in previewInstances)
+            {
+                DestroyImmediate(previewInstance);
+            }
+            previewInstances.Clear();
+        }
+
+        public void Paint()
+        {
+            foreach (var previewInstance in previewInstances)
+            {
+                previewInstance.transform.localPosition = position;
+                previewInstance.hideFlags = HideFlags.None;
+                previewInstance.tag = "Untagged";
+            }
+            previewInstances.Clear();
+            
+            // var instance = PrefabUtility.InstantiatePrefab(selectedEntry.prefab) as GameObject;
+            
+            // if (currentBrush.parent != null)
+            //     instance.transform.parent = currentBrush.parent;
+            // instance.transform.position = currentBrush.transform.position;
+        }
     }
 }
