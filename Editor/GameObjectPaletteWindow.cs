f00c3494bd42ddb2fdf837eca85c4fd9ee0b4fab
Ariel Coppes <ariel.coppes@gmail.com>
2020-11-07 19:34:51 -0300

    testing erase feature by clicking while holding left alt (should be confiurable or something)

--- Editor/GameObjectPaletteWindow.cs
+++ Editor/GameObjectPaletteWindow.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
@@ -16,7 +17,7 @@ namespace Gemserk.Tools.ObjectPalette.Editor
             window.minSize = new Vector2(300, 300);
         }
 
-        private class PaletteEntry
+        private class PaletteEntry : IEquatable<PaletteEntry>
         {
             public string name;
             public GameObject prefab;
@@ -24,9 +25,28 @@ namespace Gemserk.Tools.ObjectPalette.Editor
 
             public override bool Equals(object obj)
             {
-                if (obj is PaletteEntry e)
-                    return e.prefab.Equals(prefab);
-                return base.Equals(obj);
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((PaletteEntry) obj);
+            }
+
+            public bool Equals(PaletteEntry other)
+            {
+                if (ReferenceEquals(null, other)) return false;
+                if (ReferenceEquals(this, other)) return true;
+                return name == other.name && Equals(prefab, other.prefab) && Equals(preview, other.preview);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    var hashCode = (name != null ? name.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (prefab != null ? prefab.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (preview != null ? preview.GetHashCode() : 0);
+                    return hashCode;
+                }
             }
         }
         
@@ -46,6 +66,8 @@ namespace Gemserk.Tools.ObjectPalette.Editor
         private static readonly float buttonPreviewMaxSize = 150;
 
         private float currentButtonSize;
+
+        private bool erasing = false;
 
         private void OnEnable()
         {
@@ -101,17 +123,36 @@ namespace Gemserk.Tools.ObjectPalette.Editor
 
         private void OnSceneViewGui(SceneView sceneView)
         {
-            // Handles.BeginGUI();
-            // // var viewSize = Handles.GetMainGameViewSize();
-            // var r = sceneView.camera.pixelRect;
-            // if (GUI.Button(new Rect(r.xMax - 100, r.yMax - 100 , 75, 75),"Pipote"))
-            // {
-            //     sceneView.ShowNotification(new GUIContent("HOLA"), 2);
-            // }
-            // Handles.EndGUI();
+            Handles.BeginGUI();
+            // var viewSize = Handles.GetMainGameViewSize();
+            var r = sceneView.camera.pixelRect;
+            if (GUI.Button(new Rect(r.xMax - 100, r.yMax - 100 , 75, 75),$"Erasing:\n{erasing}"))
+            {
+                // sceneView.ShowNotification(new GUIContent("HOLA"), 2);
+            }
+            Handles.EndGUI();
             
-            if (selectedEntry == null || brush == null)
-                return;
+            if (Event.current.rawType == EventType.KeyDown && Event.current.keyCode == KeyCode.LeftAlt)
+            {
+                erasing = true;
+                UnselectUnityTool();
+
+                brush?.DestroyPreview();
+            }
+
+            if (Event.current.rawType == EventType.KeyUp && Event.current.keyCode == KeyCode.LeftAlt)
+            {
+                erasing = false;
+                RestoreUnityTool();
+
+                if (brush != null && selectedEntry != null)
+                {
+                    brush.CreatePreview(new List<GameObject>() {selectedEntry.prefab});
+                }
+            }
+            
+            // if (selectedEntry == null || brush == null)
+            //     return;
             
             var p = Event.current.mousePosition;
             // currentBrush.transform.position = p;
@@ -126,19 +167,36 @@ namespace Gemserk.Tools.ObjectPalette.Editor
 
             if (Event.current.rawType == EventType.MouseDown && Event.current.button == 0)
             {
-                // TODO: this is brush logic...
-                // var instance = PrefabUtility.InstantiatePrefab(selectedEntry.prefab) as GameObject;
-                
-                brush.Paint();
-                brush.CreatePreview(new List<GameObject>
+                if (!erasing)
                 {
-                    selectedEntry.prefab
-                });
-                
-                // if (currentBrush.parent != null)
-                //     instance.transform.parent = currentBrush.parent;
-                // instance.transform.position = currentBrush.transform.position;
-                Event.current.Use();
+                    if (selectedEntry != null && brush != null)
+                    {
+                        brush.Paint();
+                        brush.CreatePreview(new List<GameObject>
+                        {
+                            selectedEntry.prefab
+                        });
+                        Event.current.Use();
+                    }
+                }
+                else
+                {
+                    // var go = HandleUtility.PickGameObject(position, false);
+                    var go = HandleUtility.PickGameObject(p, true);
+                    
+                    if (go != null)
+                    {
+                        Debug.Log($"Erase: {go.name}");
+                    }
+                    // TODO: ignore camera, etc...
+                    if (go != null)
+                    {
+                        DestroyImmediate(go);
+                        Event.current.Use();
+                    }
+                    // raycast select object in scene?
+                    // destroy object...
+                }
             }
             
             // if (Event.current.rawType == EventType.MouseDrag)
