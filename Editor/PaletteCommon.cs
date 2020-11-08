06afef3f1d2137bf7bf9902bc50eb205be9f8e06
Ariel Coppes <ariel.coppes@gmail.com>
2020-11-08 14:29:39 -0300

    movnig logic to tool, changed to switch to erase with E key

--- Editor/PaletteCommon.cs
+++ Editor/PaletteCommon.cs
@@ -3,6 +3,7 @@ namespace Gemserk.Tools.ObjectPalette.Editor
     public static class PaletteCommon
     {
         public static PaletteToolMode mode = PaletteToolMode.Paint;
+        public static IBrush brush;
         public static PaletteSelection selection = new PaletteSelection();
     }
 }
--- Editor/PalettePaintTool.cs
+++ Editor/PalettePaintTool.cs
@@ -2,7 +2,6 @@ using UnityEditor;
 using UnityEditor.EditorTools;
 using UnityEngine;
 
-// Tagging a class with the EditorTool attribute and no target type registers a global tool. Global tools are valid for any selection, and are accessible through the top left toolbar in the editor.
 namespace Gemserk.Tools.ObjectPalette.Editor
 {
     [EditorTool("Platform Tool")]
@@ -14,9 +13,9 @@ namespace Gemserk.Tools.ObjectPalette.Editor
 
         private GUIContent m_IconContent;
 
-        void OnEnable()
+        private void OnEnable()
         {
-            m_IconContent = new GUIContent()
+            m_IconContent = new GUIContent
             {
                 image = m_ToolIcon,
                 text = "GameObject Palette Tool",
@@ -29,28 +28,62 @@ namespace Gemserk.Tools.ObjectPalette.Editor
         // This is called for each window that your tool is active in. Put the functionality of your tool here.
         public override void OnToolGUI(EditorWindow window)
         {
-            // TODO: paint using this tool, not the onscene gui
+            var p = Event.current.mousePosition;
+            var rawEvent = Event.current.rawType;
             
-            // Event.current.Use();
-            //
-            // EditorGUI.BeginChangeCheck();
-            //
-            // Vector3 position = UnityEditor.Tools.handlePosition;
-            //
-            // using (new Handles.DrawingScope(Color.green))
-            // {
-            //     position = Handles.Slider(position, Vector3.right);
-            // }
-            //
-            // if (EditorGUI.EndChangeCheck())
-            // {
-            //     Vector3 delta = position - UnityEditor.Tools.handlePosition;
-            //
-            //     Undo.RecordObjects(Selection.transforms, "Move Platform");
-            //
-            //     foreach (var transform in Selection.transforms)
-            //         transform.position += delta;
-            // }
+            if (Event.current.rawType == EventType.KeyDown && Event.current.keyCode == KeyCode.E)
+            {
+                if (PaletteCommon.mode == PaletteToolMode.Paint)
+                {
+                    PaletteCommon.mode = PaletteToolMode.Erase;
+                    PaletteCommon.brush?.DestroyPreview();
+                    Event.current.Use();
+                }
+                else
+                {
+                    PaletteCommon.mode = PaletteToolMode.Paint;
+                    if (PaletteCommon.brush != null && !PaletteCommon.selection.IsEmpty)
+                        PaletteCommon.brush.CreatePreview(PaletteCommon.selection.SelectedPrefabs);
+                    Event.current.Use();
+                }
+            }
+
+            // var isPaintEvent = rawEvent == EventType.MouseDown || rawEvent == EventType.MouseDrag || rawEvent == EventType.MouseMove;
+            
+            // TODO: repeat delay/distance.
+
+            if (rawEvent == EventType.MouseDown && Event.current.button == 0)
+            {
+                if (PaletteCommon.mode == PaletteToolMode.Paint)
+                {
+                    if (PaletteCommon.brush != null && !PaletteCommon.selection.IsEmpty)
+                    {
+                        PaletteCommon.brush.Paint();
+                        PaletteCommon.brush.CreatePreview(PaletteCommon.selection.SelectedPrefabs);
+                        // Event.current.Use();
+                    }
+                }
+                else if (PaletteCommon.mode == PaletteToolMode.Erase)
+                {
+                    var go = HandleUtility.PickGameObject(p, true);
+                    
+                    if (go != null)
+                    {
+                        Debug.Log($"Erase: {go.name}");
+                    }
+                    
+                    // Custom
+                    // d_color_picker
+                    
+                    // check if it is a prefab instance since now we instantiate prefabs (but that could change)
+                    // TODO: check if object in root, etc.
+                    if (go != null && PrefabUtility.GetPrefabInstanceStatus(go) != PrefabInstanceStatus.NotAPrefab)
+                    {
+                        Undo.DestroyObjectImmediate(go);
+                        // Event.current.Use();
+                    }
+                }
+            }
         }
     }
 }
